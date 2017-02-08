69fa7b959835e2e57d0f6c4a4514e5de99c2ecb4
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-02-08 11:35:29 -0800

    add planning images and a constructor function

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function Project(name, screenshot, description, link) {
+  this.name = name;
+  this.screenshot = screenshot;
+  this.description = description;
+  this.link = link;
+}
