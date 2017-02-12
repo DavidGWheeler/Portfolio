6fa44e186d58f0fd1786248635754816912b985a
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-02-11 18:08:44 -0800

    add improved menu icons and format

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ function Project(opts) {
   this.screenshot = opts.screenshot;
   this.imgAlt = opts.imgAlt;
   this.description = opts.description;
-  this.link = opts.link;
+  this.link = opts.projectUrl;
   this.body = opts.body;
+  this.category = opts.category;
+  this.publishedOn = opts.publishedOn;
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone().removeClass('.template');
+  $newProject.find('h1:first').text(this.title);
+  $newProject.find('img.screenshot', this.screenshot).attr('url', this.screenshot);
+  $newProject.find('img.screenshot').attr('alt', this.imgAlt);
+  $newProject.attr('data-category', this.category);
+  $newProject.find('.byline a').text(this.name);
+  $newProject.find('.byline a').attr('url', this.projectUrl)
+  $newProject.find('.article-body').html(this.body);
+  $newProject.find('time[pubdate]').attr('datetime', this.publishedOn);
+  $newProject.find('time[pubdate]').attr('title', this.publishedOn);
+  $newProject.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  return $newProject;
+};
