4a6239e285129fe21169c0d9f04084bba5cce735
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-02-12 16:41:54 -0800

    class 2 mvp

--- scripts/project.js
+++ scripts/project.js
@@ -6,29 +6,28 @@ console.log(allProjects);
 function Project(opts) {
   this.title = opts.title;
   this.category = opts.category;
-  this.name = opts.name;
+  this.projectName = opts.name;
   this.screenshot = opts.screenshot;
   this.imgAlt = opts.imgAlt;
-  this.description = opts.description;
   this.projectUrl = opts.projectUrl;
   this.body = opts.body;
   this.publishedOn = opts.publishedOn;
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone().removeClass('.template');
-  if (!this.publishedOn) $newProject.addClass('draft');
-  $newProject.find('h1:first').text(this.title);
-  $newProject.data('category', this.category);
+  var $newProject = $('article.template').clone().removeClass('template');
   $newProject.attr('data-category', this.category);
-  $newProject.find('img.screenshot', this.screenshot).attr('url', this.screenshot);
-  $newProject.find('img.screenshot').attr('alt', this.imgAlt);
-  $newProject.find('.byline a').text(this.name);
+  $newProject.find('.title').text(this.title);
+  $newProject.find('.byline a').text(this.projectName);
   $newProject.find('.byline a').attr('url', this.projectUrl)
-  $newProject.find('.article-body').html(this.body);
   $newProject.find('time[pubdate]').attr('datetime', this.publishedOn);
   $newProject.find('time[pubdate]').attr('title', this.publishedOn);
-  $newProject.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newProject.find('img.screenshot', this.screenshot).attr('url', this.screenshot);
+  $newProject.find('img.screenshot').attr('alt', this.imgAlt);
+  $newProject.find('.project-body').html(this.body);
+  // snagged from starter code
+  $newProject.find('time').text('Last update: ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  // snagged from starter code
   return $newProject;
 };
 
@@ -36,10 +35,10 @@ projectData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-projectData.forEach(function(projectObject) {
-  allProjects.push(new Project(projectObject));
+projectData.forEach(function(ele) {
+  allProjects.push(new Project(ele));
 });
 
-allProjects.forEach(function(a){
+allProjects.forEach(function(a) {
   $('#projects').append(a.toHtml());
 });
