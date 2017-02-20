25be6d5bfd45299d7d7850ba368f9e4125269676
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-02-19 21:27:15 -0800

    add more styling efforts.

--- public/scripts/projectView.js
+++ public/scripts/projectView.js
@@ -1,27 +1,37 @@
 'use strict';
 
-const projectView = {};
+(function(module) {
+  const projectView = {};
 
+  projectView.handleSubNav = function() {
+    $('.sub-nav').on('click', '.col', function() {
+      $('#' + $(this).data('content')).toggleClass('full-width');
+    });
 
-projectView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
+    $('.sub-nav .tab:first').click();
+  };
+
+  projectView.handleMainNav = function() {
+    $('.main-nav').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
+
+    $('.main-nav .tab:first').click();
+  };
+
+  // TODO: columnTeaser
+
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
   });
 
-  $('.main-nav .tab:first').click();
-};
+  projectView.initIndexPage = function() {
+    Project.all.forEach(function(a) {
+      $('#projects').append(a.toHtml())
+    });
 
-// $('#projects').append(projects.toHtml());
-
-$('pre code').each(function(i, block) {
-  hljs.highlightBlock(block);
-});
-
-projectView.initIndexPage = function() {
-  Project.all.forEach(function(a){
-    $('#projects').append(a.toHtml())
-  });
-
-  projectView.handleMainNav();
-};
+    projectView.handleMainNav();
+  };
+  module.projectView = projectView;
+})(window);
