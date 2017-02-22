9f1b42fa617ed7dd8ffcf6a04167ac4f8a328fa8
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-02-22 13:52:40 -0800

    add page.js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const express = require('express');
-
-
 const PORT = process.env.PORT || 4000;
-
 const app = express();
+
 
 app.use(express.static('./public'));
 
-app.get('*', function(request, response) {
-  response.sendFile('index.html', {root: '.'});
-});
+app.get('/', (request, response) => response.sendFile('index.html', {root: './public'}));
+app.get('/about', (request, response) => response.sendFile('index.html', {root: './public'}));
+
 
 app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}!`);
